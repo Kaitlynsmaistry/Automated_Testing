29a7e893000973538394b0a3643fbebffcd1bc96
Kaitlynsmaistry <ST10474034@myemeris.edu.za>
2026-04-07 08:41:07 +0200

    First Commit

---
+++ src/main/java/com/mycompany/automated_testing/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.automated_testing;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Employee {
+   
+    
+    public void CalculatedSalary(){
+    }
+    
+    
+}
